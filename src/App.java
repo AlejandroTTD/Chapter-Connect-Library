5955a7cb6c750a4e8de39a51d5b21bde516b30d8
AlejandroTTD <davidcuasquer21@gmail.com>
2024-08-11 15:24:37 -0500

    Update DataAccess

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import DataAccess.EntidadTipoDAO;
+import DataAccess.DTO.EntidadTipoDTO;
 import Interface.forma.MainForma;
 import Interface.forma.cambioPanel;
 public class App {
@@ -5,5 +7,13 @@ public class App {
         cambioPanel.show();
         MainForma frmMain = new MainForma("Libreria");
             frmMain.setVisible(true);
+
+        // try {
+        //     EntidadTipoDAO eTDao = new EntidadTipoDAO();
+        //     for (EntidadTipoDTO eT : eTDao.readAll())
+        //         System.out.println(eT.toString());
+        // } catch (Exception e) {
+        //     System.out.println(e.toString());
+        // }
     }
 }

ecfaed0a35b3bc06508ccca213edc99584231f0d
Celeste040205 <inu-kun_2005@hotmail.com>
2024-08-11 21:53:25 -0500

    c

--- src/Interface/forma/RegistroPanel.java
+++ src/Interface/forma/RegistroPanel.java
@@ -1,42 +1,71 @@
 package Interface.forma;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-import Interface.IAStyle;
+import javax.swing.*;
 import Interface.Customer.newButton;
+import java.awt.*;
 
-public class RegistroPanel extends JPanel{
-    public newButton
+public class RegistroPanel extends JPanel {
+    private JTextField txtUsuario, txtEmail;
+    private newButton btnNuevo, btnModificar, btnEliminar, btnGuardar, btnCancelar;
+    private JList<String> listaUsuarios;
+    private DefaultListModel<String> modeloLista;
 
-        btnNuevoRegistro         = new newButton("Nuevo"),
-        btnModificarRegistro     = new newButton("Modificar"),
-        btnEliminatr             = new newButton("Eliminar");
+    public RegistroPanel() {
+        setLayout(new BorderLayout());
 
-    public RegistroPanel(){
-        customizeComponent();
+        // Panel de entrada de datos
+        JPanel panelDatos = new JPanel(new GridLayout(3, 2, 5, 5));
+        panelDatos.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        panelDatos.add(new JLabel("Usuario:"));
+        txtUsuario = new JTextField(20);
+        panelDatos.add(txtUsuario);
+        panelDatos.add(new JLabel("Email:"));
+        txtEmail = new JTextField(20);
+        panelDatos.add(txtEmail);
+
+        JPanel panelBotones = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        btnNuevo = new newButton("Nuevo");
+        btnModificar = new newButton("Modificar");
+        btnEliminar = new newButton("Eliminar");
+        btnGuardar = new newButton("Guardar");
+        btnCancelar = new newButton("Cancelar");
+        panelBotones.add(btnNuevo);
+        panelBotones.add(btnModificar);
+        panelBotones.add(btnEliminar);
+        panelBotones.add(btnGuardar);
+        panelBotones.add(btnCancelar);
+
+        // Lista de usuarios
+        modeloLista = new DefaultListModel<>();
+        listaUsuarios = new JList<>(modeloLista);
+        JScrollPane scrollPane = new JScrollPane(listaUsuarios);
+
+        // Añadir componentes al panel principal
+        add(panelDatos, BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+        add(panelBotones, BorderLayout.SOUTH);
+
+        // Configurar acciones de los botones
+        configurarAcciones();
     }
 
-    private void customizeComponent() {
-        setBackground(IAStyle.COLOR_FONDO2);
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        setPreferredSize(new Dimension(180, 820)); 
-
-        try {
-            ImageIcon imageIcon = new ImageIcon(IAStyle.URL_FONDO);
-            add(new JLabel(imageIcon),BorderLayout.CENTER);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void configurarAcciones() {
+        btnNuevo.addActionListener(e -> limpiarCampos());
         
-        add(btnNuevoRegistro);
-        add(btnModificarRegistro);
-        add(btnEliminatr);
+        btnModificar.addActionListener(e -> {
+        });
+        
+        btnEliminar.addActionListener(e -> {
+        });
+        
+        btnGuardar.addActionListener(e -> {
+        });
+        
+        btnCancelar.addActionListener(e -> limpiarCampos());
     }
 
+    private void limpiarCampos() {
+        txtUsuario.setText("");
+        txtEmail.setText("");
+    }
 }
